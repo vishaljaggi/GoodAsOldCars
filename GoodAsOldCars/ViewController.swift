14e4bc5f681953bba8f75ab64d5a44caf4db0d2d
Vishal Jaggi <vishaljaggi@vishals-macbook-pro.local>
2017-08-05 12:08:32 -0400

    Initial Commit

---
+++ GoodAsOldCars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodAsOldCars
+//
+//  Created by Vishal Jaggi on 2017-08-05.
+//  Copyright © 2017 Vishal Jaggi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
